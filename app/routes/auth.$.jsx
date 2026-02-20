88059e5fe8e50fa4cf973181ebf9b708769c3de1
Sanjay9729 <sanjay.vaghela@appunik.com>
2026-02-20 12:21:41 +0530

    infinity issue

--- app/routes/auth.$.jsx
+++ app/routes/auth.$.jsx
@@ -1,8 +1,9 @@
 import { boundary } from "@shopify/shopify-app-remix/server";
-import { authenticate } from "../shopify.server";
+import { authenticate, registerWebhooks } from "../shopify.server";
 
 export const loader = async ({ request }) => {
-  await authenticate.admin(request);
+  const { session } = await authenticate.admin(request);
+  await registerWebhooks({ session });
 
   return null;
 };
