415a8c6a4bf022d198a44633546bb2270cbaebda
Sanjay9729 <sanjay.vaghela@appunik.com>
2026-06-09 16:03:56 +0530

    feat: enable unstable_newEmbeddedAuthStrategy for token exchange authentication

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -339,7 +339,7 @@ const shopify = shopifyApp({
   })(),
   future: {
     expiringOfflineAccessTokens: true,
-    unstable_newEmbeddedAuthStrategy: false,
+    unstable_newEmbeddedAuthStrategy: true,
   },
   ...(process.env.SHOP_CUSTOM_DOMAIN && !process.env.SHOP_CUSTOM_DOMAIN.endsWith('.myshopify.com')
     ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
